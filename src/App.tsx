3a6b377efca3c960a48cd0d09ba99cb96a475811
Navin9-Sh <ns4281692@gmail.com>
2026-01-16 14:13:30 +0530

    updated whatsapp number for floating

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
       <Sonner />
 
       <a
-  href="https://wa.me/919876543210?text=Hi%20I%20want%20to%20apply%20for%20a%20loan"
+  href="https://wa.me/8421376852?text=Hi,%20I%20want%20to%20apply%20for%20a%20loan."
   target="_blank"
   rel="noopener noreferrer"
   className="fixed bottom-6 right-6 z-50
